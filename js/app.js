34ce74d80531305b0b0628e346976d1a37f0cef4
KarinaHeras <karinaheras1847@gmail.com>
2019-03-06 22:16:32 +0100

    Primera Prueba de concepto

---
+++ js/app.js
@@ -0,0 +1,21 @@
+// practicando promesas con arrow function
+
+function getUsers(url){
+    const users = fetch(url).then(response=>response.json())
+    return users    
+}
+
+// practicando async/await con arrow function
+
+async function showResults(url, whereToPaint){
+    const users = await getUsers(url);
+    users.forEach(user => {
+        whereToPaint.innerHTML += `<li><b>${user.name}</b> can be contacted in the email: <b>${user.email}</b> </li>`    
+    });
+}
+
+const endpoint = 'https://jsonplaceholder.typicode.com/users'
+
+const paintLocation = document.querySelector('#users')
+
+showResults(endpoint, paintLocation)
